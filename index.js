0f45a56ffd9e59da1f7e66075dd2cd1d638ee94c
Daniel112300 <82494319+Daniel112300@users.noreply.github.com>
2025-06-11 10:00:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+// 要打開的 Rickroll 影片網址
+const rickrollUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXCgBwE%3D';
 
+// 偵測表單送出事件，打開 Rickroll
 document.querySelector('.login-box').addEventListener('submit', function(e) {
     e.preventDefault();
-    alert('這是一個釣魚網站');
+    window.open(rickrollUrl, '_blank');
 });
 
-
+// 偵測社群按鈕點擊事件，打開 Rickroll
 document.querySelectorAll('.social-btn').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        alert('這是一個釣魚網站');
+        window.open(rickrollUrl, '_blank');
     });
+});
+
+// 偵測 help-link 點擊，打開 Rickroll
+document.querySelector('.help-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.open(rickrollUrl, '_blank');
+});
+
+// 偵測忘記密碼，打開 Rickroll
+document.querySelector('.forgot-link').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.open(rickrollUrl, '_blank');
 });
